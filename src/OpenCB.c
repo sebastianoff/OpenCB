ec2767fd45a978e3473a6bca687e226e6f300af5
sebastianoff <cheesecakeycat@protonmail.com>
2025-11-12 07:43:51 +0300

    Add banner.

--- src/OpenCB.c
+++ src/OpenCB.c
@@ -1,3 +1,14 @@
+//-----------------------------------------------------------------------------
+//
+// This source is available for distribution and/or modification
+// only under the terms of the MIT License.
+//
+// The source is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See LICENSE.TXT in
+// root for details.
+//
+//-----------------------------------------------------------------------------
 #include <stdio.h>
 
 int main(void) {
